1b0cc45674fc71282995a87553147ab811a085ee
natividadesusana <susanajdsn@gmail.com>
2023-06-02 02:20:10 -0300

    deleting and editing posts

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,4 +1,4 @@
-import { createPostsDB, getPostsDB } from "../repositories/posts.repositories.js";
+import { createPostsDB, getPostsDB, deletePostsDB, updatePostDB } from "../repositories/posts.repositories.js";
 
 export async function getPosts(req, res) {
   try {
@@ -19,6 +19,23 @@ export async function createPosts(req, res) {
   }
 }
 
+export async function updatePosts(req, res) {
+  const { id } = req.params;
+  const { url, description } = req.body;
+  try {
+    await updatePostDB(id, url, description);
+    res.sendStatus(201); 
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
-
-
+export async function deletePosts(req, res) {
+  const { id } = req.params;
+  try {
+    await deletePostsDB(id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/repositories/posts.repositories.js
+++ src/repositories/posts.repositories.js
@@ -8,5 +8,12 @@ export function createPostsDB(url, description) {
   return db.query(`INSERT INTO posts (url, description) VALUES ($1, $2)`, [url, description]);
 }
 
-
+export function updatePostDB(id, url, description) {
+  return db.query(`UPDATE posts SET url = $1, description = $2 WHERE id = $3`, [url, description, id]);
+}
   
+
+export function deletePostsDB(id) {
+  return db.query(`DELETE FROM posts WHERE id = $1`, [id]);
+}
+
