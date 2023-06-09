a3a6368b458d9a6855b63725a4c6e78d8cf24a3a
Andre Hilario <andre18hilario@gmail.com>
2023-06-09 18:34:48 -0300

    feat: Merge follows on main

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,11 +2,14 @@ import { validateSchema } from '../middlewares/validateSchema.middleware.js'
 import { Router } from 'express'
 import { getUserName } from '../repositories/users.repositories.js'
 import { usersSchema } from '../schemas/users.schemas.js'
-import { getUserId, getUsers } from '../controllers/users.controller.js'
+import { getFollowers, getUserId, getUsers, postFollow, unfollow } from '../controllers/users.controller.js'
 
 const userRouter = Router()
 
 userRouter.get('/users/:userName', getUsers)
 userRouter.get('/user/:id', getUserId)
+userRouter.get('/users/:id/followers/:userId', getFollowers)
+userRouter.post('/users/followers', postFollow)
+userRouter.delete('/users/:id/followers/:userId', unfollow)
 
 export default userRouter
